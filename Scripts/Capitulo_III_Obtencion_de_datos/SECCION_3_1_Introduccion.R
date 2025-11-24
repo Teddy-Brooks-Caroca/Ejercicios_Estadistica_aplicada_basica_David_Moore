b28829ee6d4c39cb7104f5725942007099e2336f
Teddy_Brooks <lee.brooks.caroca@gmail.com>
2025-11-24 08:25:07 -0300

    Se agrega capitulo III

---
+++ Scripts/Capitulo_III_Obtencion_de_datos/SECCION_3_1_Introduccion.R
@@ -0,0 +1,81 @@
+ # EJERCICIOS "Estadistica básica aplicada" de David S. Moore (2005)
+
+# CAPÍTULO III "Obtención de datos"
+
+# SECCIÓN 3.1 "Introducción"
+
+# **************************************************
+# PREGUNTA 3.1 - Política y género
+# **************************************************
+
+# Se sospecha que existe una diferencia importante entre sexos en cuanto a la 
+# preferencia de voto de los ciudadanos de EE UU, teniendo las mujeres mayor 
+# predilección que los hombres por los candidatos del Partido Demócrata. 
+
+# Una socióloga selecciona una muestra de votantes de tamaño grande, mujeres 
+# y hombres, inscritos en el censo electoral. Pregunta a cada uno de ellos si 
+# votó a los demócratas o a los republicanos en las últimas elecciones al 
+# Congreso de EE UU.
+
+# (a) Este estudio, ¿es un experimento? Justifica tu respuesta.
+
+"No, es un estudio observacional, porque la investigadora no asigna tratamientos ni manipula variables. 
+Solo observa características existentes (sexo y preferencia de voto)."
+
+# (b) ¿Cuál es la variable explicativa y cuál es la variable respuesta?
+
+"
+Variable explicativa = el sexo de los votantes
+Variable de respuesta = la predilección de un partido por sobre el otro
+"
+
+# **************************************************
+# PREGUNTA 3.2 - Enseñar a leer
+# **************************************************
+
+# Una educadora quiere comparar la efectividad de un programa informático 
+# que enseña a leer con el método estándar. La educadora valora la capacidad 
+# lectora de cada uno de los niños de un grupo de segundo curso de enseñanza 
+# general básica. A continuación la educadora divide al azar al grupo en dos 
+# subgrupos. Un subgrupo utiliza habitualmente el ordenador, mientras que el 
+# otro utiliza el método estándar. Al final del curso, la educadora vuelve a 
+# valorar la capacidad lectora de los niños y compara los incrementos producidos.
+
+# (a) Este estudio, ¿es un experimento? Justifica tu respuesta.
+
+"Sí, es un experimento, porque la educadora asigna aleatoriamente a los niños a dos 
+métodos de enseñanza, manipulando deliberadamente la variable explicativa para observar 
+su efecto."
+
+# (b) ¿Cuál es la variable respuesta?
+
+"La variable respuesta es el incremento en la capacidad lectora al final del curso."
+
+# (c) ¿Cuál es la variable explicativa?
+
+"En este caso, el método de enseñanza de lectura de lectura (programa informático vs. método estándar). "
+
+# **************************************************
+# PREGUNTA 3.3 - Efectos de la propaganda
+# **************************************************
+
+# En 1940, un psicólogo llevó a cabo un experimento para estudiar el efecto de 
+# la propaganda sobre la actitud de un grupo de personas hacia un Gobierno 
+# extranjero. El psicólogo preparó una prueba para determinar la actitud de la 
+# gente hacia el Gobierno alemán y la hizo pasar a un grupo de estudiantes. 
+# Después de hacerles leer propaganda alemana durante algunos meses, volvió a 
+# pasar la prueba a los estudiantes para ver si habían cambiado su actitud.
+
+# Desgraciadamente, mientras se estaba desarrollando la prueba, Alemania 
+# invadió Francia.
+
+# Explica de forma clara por qué el efecto de la confusión impide determinar 
+# el efecto de la lectura de propaganda.
+
+"El estudio queda confundido porque entre la primera y segunda medición ocurrió un hecho histórico
+relevante (la invasión de Francia por Alemania). Este evento externo pudo influir fuertemente en la 
+actitud de los estudiantes hacia Alemania, de manera independiente a la propaganda. Por tanto, no es 
+posible distinguir si el cambio en la actitud se debe a la propaganda o a la invasión: ambas variables 
+están mezcladas (confusión)."
+
+# :::::::::::::::::::::::::::::::::::::::::::::::::::: FIN SECCIÓN ::::::::::::::::::::::::::::::::::::::::::::::::::::
